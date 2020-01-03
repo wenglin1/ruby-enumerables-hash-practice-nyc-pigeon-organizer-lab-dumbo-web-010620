8ddde4a05d1da814b0700f354021223ebf88ad8a
weng lin <wenglin@buffalo.edu>
2020-01-03 19:15:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,18 @@
 def nyc_pigeon_organizer(data)
 
   new_hash = {}
-
   data.each do |key, value|
     value.each do |attribute, names|
       names.each do |name|
         if !new_hash[name]
           new_hash[name] = {}
         end
-
         if !new_hash[name][key]
           new_hash[name][key] = []
         end
-
         new_hash[name][key] << attribute.to_s
       end
     end
   end
-
   new_hash
 end

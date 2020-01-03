0cb29e47957dbeeba252e35a2ab544bf90eb6d8c
weng lin <wenglin@buffalo.edu>
2020-01-03 19:13:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key,value|
+    value.each do |category, name|
+      name.each do |pigeon|
+        if !pigeon_list[pigeon]
+          pigeon_list[pigeon] = {}
+        end
+        if !pigeon_list[pigeon].has_key?(key)
+          pigeon_list[pigeon][key] = []
+          if value[category].include?(pigeon)
+            pigeon_list[pigeon][key].push(category.to_s)
+          end
+        elsif pigeon_list[pigeon].has_key?(key) &&
+          !pigeon_list[pigeon][key].include?(category.to_s)
+          pigeon_list[pigeon][key].push(category.to_s)
+        end
+      end
+    end
+  end
+pigeon_list
 end
